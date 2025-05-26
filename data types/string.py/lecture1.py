f62d6c7328a1f10bdc1070f9804f5f589dd3c9bd
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-26 17:09:53 +0530

    completed demo

--- data types/string.py/lecture1.py
+++ data types/string.py/lecture1.py
@@ -1,8 +1,32 @@
 print("\n In Python, a string is just a snobby list of characters with quotation marks!")
 print("Lets undersand the string data type in Python")
 str = input("share your thought ! : ")
-print("The string is: ", str)
-#accessing all cahracters in string
-print("Total letters : ",len(str))
-i = int(input("which character ? : "))
-print(str[i])
+# print("The string is: ", str)
+# #accessing any cahracters in string
+# print("Total letters : ",len(str))
+# i = int(input("which character ? : "))
+# print(str[i],"(indexing strarts from 0)")
+
+# #concatenate
+# init = "I think , you are "
+# comment = "...../ complete the sentence"
+# print(init + comment)
+# end = input()
+# print(init+end)
+# # slicing
+# print("Traversing")
+# print(str[::])
+# print("OPPOSITE :")
+# newStr = str.split() # converts into list elements
+# print(" ".join(newStr[::-1]))
+# change case
+case = input("write a sentence in mix case : ")
+change = ""
+for el in case:
+    if el.islower():
+        change += el.swapcase()
+    elif el.isupper():
+        change += el.swapcase()
+    else:
+        change += " "
+print(change)
