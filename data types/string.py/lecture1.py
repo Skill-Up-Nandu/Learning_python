97f917224b6a400fd3248242c21603ece54905d6
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-26 16:13:18 +0530

    string updated

--- data types/string.py/lecture1.py
+++ data types/string.py/lecture1.py
@@ -1,6 +1,6 @@
 print("\n In Python, a string is just a snobby list of characters with quotation marks!")
 print("Lets undersand the string data type in Python")
-str = "Hello World"
+str = input("share your thought ! : ")
 print("The string is: ", str)
 #accessing all cahracters in string
 print("Total letters : ",len(str))
