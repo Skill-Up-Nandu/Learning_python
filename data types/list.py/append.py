f83241c892b0a49c7350f3c2096f0c38768e99a0
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-28 17:15:17 +0530

    add append

---
+++ data types/list.py/append.py
@@ -0,0 +1,13 @@
+# Append and Insert:
+# Add "cherry" at the end and "apple" at index 1 in the list fruits = ['banana', 'orange'].
+
+fruits = ['banana','orange']
+print("List of fruits : ",fruits)
+
+# added an element (append)
+fruits.append('cherry')
+print("After append : ",fruits)
+
+# add element at particular position
+fruits.insert(1,"apple")
+print("After adding new fruits :",fruits)
