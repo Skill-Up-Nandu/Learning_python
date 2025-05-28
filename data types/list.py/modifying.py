8435d85928418d2cc4857ebbd62f08be31aac76a
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-28 17:08:43 +0530

    modifying list

---
+++ data types/list.py/modifying.py
@@ -0,0 +1,9 @@
+# Modify List:
+# Change the third element of a list colors = ['red', 'blue', 'green'] to 'yellow'.
+
+colors = ['red','blue','green']
+print("Old List : ",colors)
+
+# list is a mutable datatype
+colors[2] = 'yellow'  # updated the third element
+print("New List :",colors)
--- data types/list.py/slicing.py
+++ data types/list.py/slicing.py
@@ -1,2 +1,9 @@
 # List Slicing:
 # Print the middle three elements of the list nums = [1, 2, 3, 4, 5, 6, 7].
+
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,18]
+count = len(nums)
+position = (count-1) // 2 # ineteger division for middle position
+
+# slicing to get the middle three elements
+print("Middle three elements are : ",nums[position-1:position+2])
