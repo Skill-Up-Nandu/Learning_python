fb4b0629759344b4583f81df4f586b20c5813483
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-28 16:49:37 +0530

    access element

--- data types/list.py/access_element.py
+++ data types/list.py/access_element.py
@@ -3,10 +3,11 @@
 
 numbers = [10,20,30,40,50,80,90,40,60]
 
+# gives elements without a list[]
+print("First element is :",numbers[0])  # numbers[0] gives the first element.
+print("Last element is :",numbers[-1])  # numbers[-1] gives the last element.
+
 # gives elements within a list[]
 print("First element is :",numbers[:1])
 print("Last element is :",numbers[-1:])
 
-# gives elements without a list[]
-print("First element is :",numbers[0])  # numbers[0] gives the first element.
-print("Last element is :",numbers[-1])  # numbers[-1] gives the last element.
---
+++ data types/list.py/slicing.py
@@ -0,0 +1,2 @@
+# List Slicing:
+# Print the middle three elements of the list nums = [1, 2, 3, 4, 5, 6, 7].
