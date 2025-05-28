b31fcee4eac1fd78b0a36dd6b22c5e0dafaca34a
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-28 16:47:59 +0530

    accessing element

---
+++ data types/list.py/access_element.py
@@ -0,0 +1,12 @@
+# Access Elements:
+# Given a list numbers = [10, 20, 30, 40, 50], print the first and last elements.
+
+numbers = [10,20,30,40,50,80,90,40,60]
+
+# gives elements within a list[]
+print("First element is :",numbers[:1])
+print("Last element is :",numbers[-1:])
+
+# gives elements without a list[]
+print("First element is :",numbers[0])  # numbers[0] gives the first element.
+print("Last element is :",numbers[-1])  # numbers[-1] gives the last element.
