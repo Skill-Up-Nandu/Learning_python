9959491cda0507bd9bb702433acd6d930954a02b
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-30 12:53:51 +0530

    add comments

--- data types/list.py/comprehensoin.py/frist_letter.py
+++ data types/list.py/comprehensoin.py/frist_letter.py
@@ -2,7 +2,8 @@
 # ➤ Given words = ['apple', 'banana', 'cherry'], make a list of the first letter of each word.
 
 words = ['apple', 'banana', 'cherry']
+print('Oroginal :',words)
 
 # retriving first chracter of each element in list
 init = [ word[0] for word in words ]
-print(init)
+print('Initials :',init)
---
+++ data types/list.py/comprehensoin.py/lenght.py
@@ -0,0 +1,3 @@
+# Length of words
+# ➤ From words = ['hello', 'world'], create a list of word lengths.
+
