2fe1e9e65953c330b9e8ea21da887adbdd5492a4
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-30 21:09:03 +0530

    comment

--- data types/list.py/comprehensoin.py/2d flatten.py
+++ data types/list.py/comprehensoin.py/2d flatten.py
@@ -3,10 +3,10 @@
 
 two_d_list = [[1, 2], [3, 4], [5, 6]]
 
-# print the all items of list
+# show the original 2d list
 flatted = [item for item in two_d_list]
 print('Original List : ',flatted)
 
-# print 2d list as fattern
+# flatten the 2d list into a 1d list
 flatted_2d = [item for sublist in two_d_list for item in sublist]
 print('flatted List :',flatted_2d)
