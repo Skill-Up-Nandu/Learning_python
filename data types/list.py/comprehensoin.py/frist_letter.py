38ba0dca0e72c3165e42daf45024b84df0a6b5d1
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-30 12:50:22 +0530

    code generated

---
+++ data types/list.py/comprehensoin.py/frist_letter.py
@@ -0,0 +1,8 @@
+# First letters of words
+# ➤ Given words = ['apple', 'banana', 'cherry'], make a list of the first letter of each word.
+
+words = ['apple', 'banana', 'cherry']
+
+# retriving first chracter of each element in list
+init = [ word[0] for word in words ]
+print(init)
