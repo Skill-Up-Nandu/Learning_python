6dcf63098bf582d1771cc055c2dc08974bd3a78f
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-05-30 12:58:48 +0530

    code generation

--- data types/list.py/comprehensoin.py/lenght.py
+++ data types/list.py/comprehensoin.py/lenght.py
@@ -1,3 +1,9 @@
 # Length of words
 # ➤ From words = ['hello', 'world'], create a list of word lengths.
 
+words = ['hello', 'world','nandini']
+print("List :" ,words)
+
+# print the length of elements
+lengths = [len(word) for word in words]
+print("Length List : ",lengths)
