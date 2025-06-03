cbcaabcee958e4613a64f59f78460e3f8c80f549
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-03 13:00:12 +0530

    leght of tuple

---
+++ 3_Data_Types/3_tuple.py/easy level/lenght.py
@@ -0,0 +1,8 @@
+# Tuple Length
+# Write a program to print the number of elements in this tuple:
+# colors = ('red', 'green', 'blue', 'yellow')
+
+colors = ('red' , 'green' ,'blue' , 'yellow')
+# calculate the length of tuple
+length = len(colors)
+print(f"Total Items In The Tuple are : {length}")
