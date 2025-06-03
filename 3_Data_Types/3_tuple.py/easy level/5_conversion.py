e5a35b21908571108b1e002c38163a743930fde9
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-03 13:21:11 +0530

    convert list into tuple

---
+++ 3_Data_Types/3_tuple.py/easy level/5_conversion.py
@@ -0,0 +1,10 @@
+# Convert List to Tuple
+# Convert this list into a tuple: my_list = [1, 2, 3, 4]
+
+my_list = [1,2,3,4]
+print(f"MY List : {my_list}")
+
+# converting into tuple
+my_tuple = tuple(my_list)
+
+print(f"AFTER CONVERSION : {my_tuple}")
