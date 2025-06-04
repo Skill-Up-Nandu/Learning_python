bac05fdae5655140c561d1393315cbb361a708e4
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-04 11:36:23 +0530

    swap tuples

---
+++ 3_Data_Types/3_tuple.py/hard level/1_swap_tuple.py
@@ -0,0 +1,15 @@
+# SWAP TUPLES
+# Swap the content of two tuples : a=(1,2) b=(3,4)
+
+a = (1, 2)
+b = (3, 4)
+print("BEFORE ->")
+print(f"a is {a}")
+print(f"b is {b}")
+
+# SWAP THE TUPLES
+b, a = a, b
+
+print("\nAFTER ->")
+print(f"a is {a}")
+print(f"b is {b}")
