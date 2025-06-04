888bd4060578cad60c25ef0c24eb6037be22caa7
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-04 12:39:09 +0530

    add try except

--- 3_Data_Types/3_tuple.py/hard level/2_return_from_function.py
+++ 3_Data_Types/3_tuple.py/hard level/2_return_from_function.py
@@ -7,16 +7,23 @@ def create_tuple(a, b):
     return (a + b, a * b)
 
 # user input
-val_1 = int(input("Enter First number : "))
-val_2 = int(input("Enter Second number : "))
+def get_data():
+    try: 
+        val_1 = int(input("\nEnter First number : "))
+        val_2 = int(input("Enter Second number : "))
+    except ValueError:
+        print("Invalid Input.")
+        return
 
-# function call
-print(f"\nValue Passes : {val_1}, {val_2}")
-result = create_tuple(val_1, val_2)
-print(f"My Tuple : {result}")
+    # function call
+    print(f"\nValue Passes : {val_1}, {val_2}")
+    result = create_tuple(val_1, val_2) 
+    print(f"My Tuple : {result}")
 
-# unpack tuple for more understnding
-sum, prod = result
+    # unpack tuple for more understnding
+    sum, prod = result
+    print(f"\nSum of {val_1} and {val_2} : {sum}")
+    print(f"Product of {val_1} and {val_2} : {prod}")
 
-print(f"\nSum of {val_1} and {val_2} : {sum}")
-print(f"Product of {val_1} and {val_2} : {prod}")
+get_data()
+
