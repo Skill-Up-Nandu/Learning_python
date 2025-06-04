1230f32a1f32f93492d072c2a42c42b960c7d56b
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-04 17:23:58 +0530

    correct the logic

--- 3_Data_Types/3_tuple.py/project.py/report_card.py
+++ 3_Data_Types/3_tuple.py/project.py/report_card.py
@@ -1,3 +1,29 @@
 # STUDENT REPORT CRAD GENERATOR USING PYTHON TUPLE
 
-# This is a beginner-friendly Python project that demonstrates how to use TUPLES effectively to store, unpack and process structured data. The project simulates a simple Report_Card_Generator using a list of student records stored as TUPLES.
+# 📄 Report Card for Alice (Roll No: 101)
+# - Scores: Math: 85, Science: 90, English: 78
+# - Average: 84.33
+# - Hobbies: painting, reading
+
+students = [
+    ("Alice", (85, 90, 78), 101, "painting", "reading"),
+    ("Bob", (72, 88, 91), 102, "chess"),
+    ("Charlie", (95, 92, 89), 103, "football", "gaming")
+]
+print("\nStudents Report Card Of Class - Xth\n")
+max_avg = 0
+topper_name = ""
+
+for name, score, enroll , *hobbies in students:
+    print(f"\n📄 Report Card for {name} (Roll No: {enroll})")
+    math, sci, eng = score
+    print(f"- Scores : Maths : {math}, Science : {sci}, English : {eng}")
+    avg = (sum(score)/len(score))
+    print(f"- Average : {avg:.2f}")
+    print(f"- Hobbies : {', '.join(hobbies)}")
+    if avg >= max_avg:
+        max_avg = avg
+        topper_name = name
+
+print("\n--------------------------------------------\n")
+print(f"✅ Class Topper : {topper_name} (Average {max_avg})\n")
