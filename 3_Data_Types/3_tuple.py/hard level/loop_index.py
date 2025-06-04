694cb9f484fb04dfc2d25f0341d6559b9c69625a
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-04 12:08:02 +0530

    loop through tuple with index

---
+++ 3_Data_Types/3_tuple.py/hard level/loop_index.py
@@ -0,0 +1,7 @@
+# Loop Through Tuple with Index
+# Print each item in the tuple with its index: items = ("pen", "pencil", "eraser")
+
+items = ("pen", "pencil", "eraser")
+
+for idx , item in enumerate(items):
+    print(f"Index {idx} : {item}")
