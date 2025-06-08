b1ba95bc1c6762d53980b623f226e090e160efec
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-08 17:00:53 +0530

    nested dictionary

---
+++ 3_Data_Types/4_dictionary.py/practice_questions.py/nested_dic.py
@@ -0,0 +1,10 @@
+# Create a dictionary of students where each value is another dictionary with keys: "marks", "grade", and "city".
+
+students_record = {
+    'nandini' : { 'marks' : 85, 'grade' : 'b', 'city' : 'delhi'},
+    'arnish' : { 'marks' : 74, 'grade' : 'c', 'city' : 'ambala'},
+    'ishu' : { 'marks' : 96, 'grade' : 'a', 'city' : 'indore'},
+    'naina' : { 'marks' : 89, 'grade' : 'a', 'city' : 'pune'}
+}
+
+print(students_record['nandini']['grade'])
