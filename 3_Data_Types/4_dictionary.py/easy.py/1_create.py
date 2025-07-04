4767eb8e930f1baeb4bd55818963e6a330679be7
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-07-04 20:15:17 +0530

    create and print dictionery

---
+++ 3_Data_Types/4_dictionary.py/easy.py/1_create.py
@@ -0,0 +1,18 @@
+# Create a dictionary with the following key-value pairs:
+# Name: "Alice", Age: 25, City: "Mumbai" 
+
+person = {
+    "name" : "naina",
+    "age" : 25,
+    "city" : "Mumbai"
+}
+
+print(f"\nMy_dcit : {person}\n")
+
+print(f"In Details : ")
+print(f"Name : {person["name"]}")
+print(f"Age : {person["age"]}")
+print(f"City : {person["city"]}\n")
+
+print(f"Details in OneLine : ")
+print(f"Name: {person['name']}, Age: {person['age']}, City: {person['city']}")
