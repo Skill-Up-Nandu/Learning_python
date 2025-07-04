3831347c0ef94727748961f5114d7f634a17e461
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-07-04 20:17:49 +0530

    access the value associated with the key

---
+++ 3_Data_Types/4_dictionary.py/easy.py/2_access.py
@@ -0,0 +1,10 @@
+# Access the value associated with the key "City" from the dictionary.
+
+
+person = {
+    "name" : "naina",
+    "age" : 25,
+    "city" : "Mumbai"
+}
+
+print(person.get("city"))
