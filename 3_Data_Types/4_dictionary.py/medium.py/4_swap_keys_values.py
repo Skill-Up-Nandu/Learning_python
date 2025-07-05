a4dc3fe292f87a25d7af1208e33f7f1bbece6448
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-07-05 17:35:20 +0530

    swap key and values using comprehension

--- 3_Data_Types/4_dictionary.py/medium.py/4_swap_keys_values.py
+++ 3_Data_Types/4_dictionary.py/medium.py/4_swap_keys_values.py
@@ -1,3 +1,18 @@
+# Write a program to swap keys and values in a dictionary.
+
+person = {
+    "name" : "Naina" ,
+    "age" : 27,
+    "city" : "Ambala" ,
+    "Profession" : "HR Associative"
+}
+
+def swap_key_value():
+    swapped = {v : k for k , v in person.items()}
+    print(f"Swapped : {swapped}")
+
+swap_key_value()
+
 
 
 
