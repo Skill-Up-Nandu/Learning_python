eda72e8ad5a00b6d93c004850c067e5f568b7a3e
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-07-05 15:06:19 +0530

    one more method update of finding max values

--- 3_Data_Types/4_dictionary.py/medium.py/3_max_value.py
+++ 3_Data_Types/4_dictionary.py/medium.py/3_max_value.py
@@ -5,7 +5,12 @@ scores = {'A': 78, 'B': 82, 'C': 91}
 def check_maximum() :
     max_key = max(scores , key = scores.get )
     max_value = scores.get(max_key)
-    print(f"Maximum Score : {max_value}")
+    print(f"Maximum Score : {max_value} aggregate by {max_key}")
+
+def check_max():
+    max_val = max(scores.values())
+    print(f"Maximum Score : {max_val}")
 
 check_maximum()
+check_max()
 
