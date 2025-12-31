c7f9d52f9369f849449487cbb7743bca6b2f037d
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2025-12-31 14:38:31 +0530

    performd dot multiplication over arrays

---
+++ numpy_library/9_operations/3_matrix_operaitons.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# dot product (multipy between two matrix)
+# condition  row_size of arr1 == column_size of arr2 and vice versa
+
+arr1 = np.array([[1,2],
+                [3,4],
+                [5,6]])
+arr2 = np.array([[7,8,9],
+                [10,11,12]])
+
+mul = arr1 @ arr2
+print(mul)
+
+dot = np.dot(arr1,arr2)
+print(f"\n{dot}")
