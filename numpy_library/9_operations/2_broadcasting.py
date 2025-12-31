a6a7ea1d47728b55013d6bfe4ed1703b8e05394f
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2025-12-31 14:23:46 +0530

    Broadcasting of a vector

---
+++ numpy_library/9_operations/2_broadcasting.py
@@ -0,0 +1,21 @@
+# As similar to arithmetic operation but here we braodcast all elements of one array using arithmetic operators
+
+import numpy as np
+arr = np.arange(10,21)
+
+print(f"Array : {arr}")
+
+arr = arr ** 2
+
+print(f"Broadcast Array : {arr}")
+
+
+# another example
+
+marks = np.array([450,856,954,365,745,922,487,555])
+
+percentage = marks / 1000 * 100
+
+print(f"Percentage of all students")
+for idx , per in enumerate(percentage,1) :
+    print(f"{idx}. {per.round(2)}%")
