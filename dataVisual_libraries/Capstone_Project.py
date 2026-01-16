483f59bc09442856854fbb169ba3f443045d628c
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2026-01-16 21:20:47 +0530

    which team won the most matches

---
+++ dataVisual_libraries/Capstone_Project.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('IPL.csv')
+# print(f"DataSet Has {df.shape[0]} Rows And {df.shape[1]} Columns in Total")
+
+# Now let's see how many columns have null values in total.
+# print(df.isnull().sum())
+
+# Which team won the most matches?
+match_wins = df['match_winner'].value_counts()
+sns.barplot(y= match_wins.index , x = match_wins.values,palette='rainbow')
+plt.title('Match Winning Teams')
+plt.tight_layout()
+plt.show()
