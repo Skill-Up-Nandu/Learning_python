a8459f77af45d225ae9f9f0151004b5ec9e2bca7
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2026-01-31 16:08:46 +0530

    independent and events

--- Probability TestCases/05(without_replacement).py
+++ Probability TestCases/05(without_replacement).py
@@ -1,11 +1,14 @@
 # Question 5: Independent Again
-
 # Same bag:
-
 # 3 red
-
 # 2 blue
-
 # Two balls are drawn with replacement.
-
 # Probability of both being red?
+
+red = 3
+blue = 2
+total = red + blue
+first = red/total
+second = red/total
+prob = first * second *100
+print(f"Probability : ~{prob:.2f}%")
